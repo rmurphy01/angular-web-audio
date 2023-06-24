c03dcc3c14388de6f3055f437915041390724c07
Robert Murphy <rmurphy@istation.com>
2023-06-24 11:52:14 -0500

    App module: add WebAudioComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WebAudioComponent } from './web-audio-component/web-audio-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebAudioComponent
   ],
   imports: [
     BrowserModule
